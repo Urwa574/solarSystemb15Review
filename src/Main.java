8b094eb4007d4ba373ca8b34e11c1ac474cada38
Urwa574 <urwawaseem574@gmail.com>
2023-03-30 01:06:56 +0500

    the basic solar system is ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       solarSystem solar=new solarSystem();
+       solar.moon="1";
+       solar.sun="2";
+       solar.planet="1";
+
+    }
+}
